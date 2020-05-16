4887270eb8b167c7aa69c56fa86ebd3378dda9ff
Vincent <nglequocviet@gmail.com>
2020-05-17 03:30:59 +0700

    bug_fix

--- models/resfcn120.py
+++ models/resfcn120.py
@@ -161,12 +161,12 @@ class Bottleneck(nn.Module):
 			norm_layer = nn.BatchNorm2d
 		width = int(planes * (base_width / 64.)) * groups
 		# Both self.conv2 and self.downsample layers downsample the input when stride != 1
-		self.conv1 = conv1x1(inplanes, width)
+		self.conv1 = nn.Conv2d(inplanes, width, kernel_size=1, stride=1, bias=False)
 		self.bn1 = norm_layer(width)
 		self.conv2 = conv3x3(width, width, stride, groups, dilation)
 		self.bn2 = norm_layer(width)
-		self.conv3 = conv1x1(width, planes * self.expansion)
-		self.bn3 = norm_layer(planes * self.expansion)
+		self.conv3 = nn.Conv2d(width, planes * 4, kernel_size=1, stride=1, bias=False)
+		self.bn3 = norm_layer(planes * 4)
 		self.relu = nn.ReLU(inplace=True)
 		self.downsample = downsample
 		self.stride = stride
